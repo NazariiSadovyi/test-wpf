53fc2e42e945577047f55214328400cce6238a6a
Nazarii Sadovyi <nazarii.sadovyi@eleks.com>
2021-10-09 22:08:45 +0300

    Net Framework WPF

--- WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/MainWindow.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace WpfApp4
+namespace WpfApp1
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
